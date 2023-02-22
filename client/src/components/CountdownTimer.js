673e05816614be7309ba941efb66d159e4eb15a3
kovalen1618 <kovalen1.618@gmail.com>
2023-02-22 17:47:07 -0600

    Successfully added time to localStorage

--- client/src/components/CountdownTimer.js
+++ client/src/components/CountdownTimer.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect, useRef, useImperativeHandle, forwardRef } f
 
 export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComplete }, ref ) {
     // Creating initial state with 60 seconds to work from
-    const [time, setTime] = useState(startingMinutes * 60);
+    const [time, setTime] = useState(() => {
+        const storedTime = localStorage.getItem('remainingTime');
+        const time = new Date(storedTime).getTime();
+        return time !== null ? parseInt(storedTime) : startingMinutes * 60;
+    });
     const intervalRef = useRef();
 
     useEffect(() => {
@@ -11,15 +15,6 @@ export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComp
             onTimerComplete();
         }
     }, [time, onTimerComplete])
-
-    const hours = Math.floor(time / 3600);
-    let minutes = Math.floor((time % 3600) / 60);
-    let seconds = time % 60;
-
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-
-    const countdown = time >= 0 ? `${hours}:${minutes}:${seconds}` : 'Expired';
 
     useImperativeHandle(ref, () => ({
             play: () => {
@@ -30,6 +25,27 @@ export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComp
         },
         pause: () => clearInterval(intervalRef.current)
     }));
+
+    // Local storage
+    useEffect(() => {
+        localStorage.setItem('remainingTime', time);
+    }, [time])
+
+    useEffect(() => {
+        const storedTime = localStorage.getItem('remainingTime');
+        if (storedTime) {
+            setTime(parseInt(storedTime));
+        }
+    }, [])
+
+    const hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time % 3600) / 60);
+    let seconds = time % 60;
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    const countdown = time >= 0 ? `${hours}:${minutes}:${seconds}` : 'Expired';
   
     // Return component to App.js
     return <div>{countdown}</div>
